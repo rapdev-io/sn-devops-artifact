bfa9062f03d6e155b0a320e7f3a8a1a4adc3616a
thorntonmc <mcthornton5@gmail.com>
2021-06-21 06:06:42 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,49 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+(async function main() {
+    const instanceName = core.getInput('instance-name', { required: true });
+    const toolId = core.getInput('tool-id', { required: true });
+    const username = core.getInput('devops-integration-user-name', { required: true });
+    const pass = core.getInput('devops-integration-user-pass', { required: true });
+    const name = core.getInput('name', { required: true });
+    const defaultHeaders = { 'Content-Type': 'application/json' };
+
+
+    let artifacts;
+    if (!!core.getInput('artifacts'), { required: true }) {
+        try {
+            artifacts = JSON.parse(core.getInput('artifacts'), { required: true });
+        } catch (e) {
+            core.setFailed(`failed to parse artifacts JSON: ${e}`);
+        }
+    }
+
+    let githubContext;
+    if (!!core.getInput('github-context'), { required: true }) {
+        try {
+            githubContext = JSON.parse(core.getInput('github-context'), { required: true });
+        } catch (e) {
+            core.setFailed(`failed to parse github context JSON: ${e}`);
+        }
+    }
+    const sncArtifactURL = `https://${username}:${pass}@${instanceName}.service-now.com/api/sn_devops/devops/artifact/registration?orchestrationToolId=${toolId}`
+
+    try {
+        let artifactBody = {
+            'artifacts': artifacts,
+            'name': name,
+            'pipelineName': `${githubContext.workflow}`,
+            'stageName': `${githubContext.job}`,
+            'taskExecutionNumber': `${githubContext.run_number}`
+        };
+
+        let artifactConfig = { headers: defaultHeaders };
+
+        await axios.post(sncArtifactURL, artifactBody, artifactConfig);
+    }
+    catch (e) {
+        core.setFailed(`failed POSTing artifact registration: ${e}`)
+    }
+
+})();
